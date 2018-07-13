369778b1522ea8f56e1d46d16dc27d8acb0a0e45
Veronika_Golikova <golikova.veronika.igorevna@mail.ru>
2018-07-13 21:41:58 +0400

    Чтение файла и анализ входящих в него значений.

---
+++ src/com/company/File1.java
@@ -0,0 +1,50 @@
+package com.company;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+public class File1 {
+    public static void main(String[] args) {
+
+        String line = new String();
+        try {
+            Scanner scanner = new Scanner(new FileReader(new File("C:\\Users\\Ni4ka\\Documents\\text.txt")));
+            line = scanner.nextLine();
+
+            System.out.println("Исходный файл: " + line);
+
+        } catch (IOException exception) {
+            System.out.println(exception);
+        }
+
+        String[] stringArr = (line.split(" "));
+        Arrays.sort(stringArr);
+
+        System.out.printf("Отсортированный список: %s",
+                Arrays.toString(stringArr) + "\n");
+
+        HashMap<String, Integer> wordToCount = new HashMap<>();
+        for (String word : stringArr) {
+            if (!wordToCount.containsKey(word)) {
+                wordToCount.put(word, 0);
+            }
+            wordToCount.put(word, wordToCount.get(word) + 1);
+        }
+        for (String word : wordToCount.keySet()) {
+            System.out.println(word + " " + wordToCount.get(word));
+        }
+        int maxValueInMap = (Collections.max(wordToCount.values()));
+        for (Map.Entry<String, Integer> entry : wordToCount.entrySet()) {
+            if (entry.getValue() == maxValueInMap) {
+                System.out.println("Cлово с максимальным количеством повторений - " + entry.getKey() + ", количество повторений - " + entry.getValue());
+            }
+        }
+    }
+}
+
+
+
+
+
